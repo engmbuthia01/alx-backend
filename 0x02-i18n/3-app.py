561263ba0018ad49e21e378f3ff7428e4fa2bf2a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-11-09 08:19:36 +0000

    transitions

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -38,7 +38,7 @@ def index():
     """
     renders a basic html template
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
